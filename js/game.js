c9fea33906d62e3b5be23ca678c804312a9dd63c
AJ Beckner <ajbeckner@gmail.com>
2016-06-28 13:27:59 -0400

    stuff and things numero uno

---
+++ js/game.js
@@ -0,0 +1,22 @@
+// RequestAnimFrame: a browser API for getting smooth animations
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame   ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			window.oRequestAnimationFrame      ||
+			window.msRequestAnimationFrame     ||
+		function( callback ){
+			return window.setTimeout(callback, 1000 / 60);
+		};
+})();
+
+window.cancelRequestAnimFrame = ( function() {
+	return  window.cancelAnimationFrame          ||
+			window.webkitCancelRequestAnimationFrame ||
+			window.mozCancelRequestAnimationFrame    ||
+			window.oCancelRequestAnimationFrame      ||
+			window.msCancelRequestAnimationFrame     ||
+		clearTimeout;
+} )();
+
+console.log('Holla');
